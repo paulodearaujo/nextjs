56b29595e42c381552c1baa78612b63493d1a70f
paulodearaujo <paulo.alexandre@cloudwalk.io>
2024-05-17 20:09:52 -0300

    V1

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,7 @@ export const authorizeWebflow = () => {
     }
 
     const encodedRedirectUri = encodeURIComponent(redirectUri);
-    const scopes = encodeURIComponent('ms:read cms:write');
+    const scopes = encodeURIComponent('cms:read cms:write');
     const authUrl = `https://webflow.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodedRedirectUri}&scope=${scopes}`;
 
     window.location.href = authUrl;
