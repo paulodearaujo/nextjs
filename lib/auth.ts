b895c627149c032502ca236606b2010d1538e7b7
paulodearaujo <paulo.alexandre@cloudwalk.io>
2024-05-17 20:16:01 -0300

    V1

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,9 +6,8 @@ export const authorizeWebflow = () => {
         throw new Error('Missing environment variables: NEXT_PUBLIC_WEBFLOW_CLIENT_ID or NEXT_PUBLIC_REDIRECT_URI');
     }
 
-    const encodedRedirectUri = encodeURIComponent(redirectUri);
-    const scopes = encodeURIComponent('cms:read cms:write');
-    const authUrl = `https://webflow.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodedRedirectUri}&scope=${scopes}`;
+    const scopes = 'cms:read cms:write';
+    const authUrl = `https://webflow.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}`;
 
     window.location.href = authUrl;
 };
