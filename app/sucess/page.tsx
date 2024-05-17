1e56fcc0f77b0bae5fe309713cf51f950b156886
paulodearaujo <paulo.alexandre@cloudwalk.io>
2024-05-17 20:38:13 -0300

    V1

--- app/sucess/page.tsx
+++ app/sucess/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {useEffect, useState} from 'react';
-import {useRouter} from 'next/router';
+import {useRouter} from 'next/navigation';
 
 const SuccessPage = () => {
     const [accessToken, setAccessToken] = useState<string | null>(null);
