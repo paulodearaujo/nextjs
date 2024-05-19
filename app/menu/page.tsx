625f445c6720f7c83242c27c5737cdf2c93abb00
paulodearaujo <paulo.alexandre@cloudwalk.io>
2024-05-19 00:48:28 -0300

    Updated UI

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,8 +4,8 @@ import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/compo
 
 const MenuPage = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <Card className="w-full max-w-4xl mx-auto shadow-lg rounded-lg bg-gray-800">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      <Card className="w-full max-w-4xl mx-auto shadow-lg rounded-lg bg-gray-800 text-white">
         <CardHeader>
           <CardTitle className="text-2xl font-bold text-center mb-4">Menu</CardTitle>
           <CardDescription className="text-center mb-4">
@@ -14,10 +14,10 @@ const MenuPage = () => {
         </CardHeader>
         <CardContent className="flex justify-center space-x-4">
           <Link href={{ pathname: '/identify-hyperlinks' }}>
-            <Button className="bg-blue-500 text-white hover:bg-blue-600">Identify existing hyperlinks</Button>
+            <Button className="bg-gray-700 text-white hover:bg-gray-600">Identify existing hyperlinks</Button>
           </Link>
           <Link href={{ pathname:'/discover-opportunities' }}>
-            <Button className="bg-blue-500 text-white hover:bg-blue-600">Discover hyperlink opportunities</Button>
+            <Button className="bg-gray-700 text-white hover:bg-gray-600">Discover hyperlink opportunities</Button>
           </Link>
         </CardContent>
       </Card>
